0205319738a2712c7e9ba64c1961f7e768fd97d7
Anderson Jahir Arrieta Poveda <anderson.arrieta@AJOVER.COM>
2026-04-29 10:36:25 -0500

    Usar plantilla operaciones/login.html en LoginView

--- seguimiento_transportistas/urls.py
+++ seguimiento_transportistas/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path(
         "login/",
         LoginView.as_view(
-            template_name="registration/login.html",
+            template_name="operaciones/login.html",
             redirect_authenticated_user=True
         ),
         name="login"
@@ -22,3 +22,4 @@ urlpatterns = [
 
     path("", include("operaciones.urls")),
 ]
+
